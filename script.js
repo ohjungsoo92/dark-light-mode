49c8838c43995f4c47562aec1ae1a7dbd92f3bb3
ohjungsoo92 <57598566+ohjungsoo92@users.noreply.github.com>
2022-02-21 21:51:24 +0900

    Update script.js
    edit address of img

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ let textBoxBgColor = textBox.style.backgroundColor;
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `../img/undraw_proud_coder_${color}.svg`;
-    image2.src = `../img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `../img/undraw_conceptual_idea_${color}.svg`;
+    image1.src = `./img/undraw_proud_coder_${color}.svg`;
+    image2.src = `./img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
 }
 
 // Dark Mode function
