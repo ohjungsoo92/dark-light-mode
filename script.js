0e83f69c61a88fe44cb705a5d3f7d01b60f5f31f
benjamin <ohjungsoo92@gmail.com>
2022-02-26 19:48:45 +0900

    edit light mode

--- script.js
+++ script.js
@@ -6,15 +6,14 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-// Dry Code
 let navBgColor = nav.style.backgroundColor;
 let textBoxBgColor = textBox.style.backgroundColor;
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `../img/undraw_proud_coder_${color}.svg`;
-    image2.src = `../img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `../img/undraw_conceptual_idea_${color}.svg`;
+    image1.src = `./img/undraw_proud_coder_${color}.svg`;
+    image2.src = `./img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
 }
 
 // Dark Mode function
